af1a90aaac88ced44c9be2bd50378fe9af7d1f9e
WKNick <150284814+WKNick@users.noreply.github.com>
2024-04-24 14:22:48 -0400

    Init

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+
+
+use panic_halt as _; // used to allow rust to run so it knows what to do if program crashes
+
+
+use cortex_m_rt::entry;// gives access to main function
+
+use MSPM0L1306_HAL;
+
+
+
+#[entry]
+fn main()->!{
+
+
+
+
+loop{
+
+}
+}
+
+
